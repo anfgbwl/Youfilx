5058fd0d009ea314b87f8cc25995e73c330d213a
EyeGrampus <139723584+EyeGrampus@users.noreply.github.com>
2023-09-06 11:03:26 +0900

    feat: 비디오 제작자 관련 뷰

---
+++ Youfilx/Feature/And.swift
@@ -0,0 +1,20 @@
+//
+//  And.swift
+//  Youfilx
+//
+//  Created by hong on 2023/09/06.
+//
+
+import Foundation
+
+public protocol And {}
+
+extension And {
+    @inlinable
+    public func and(_ block: ((Self) throws -> Void)) rethrows -> Self {
+        try block(self)
+        return self
+    }
+}
+
+extension NSObject: And {}
--- Youfilx/ViewController/DetailPageViewController.swift
+++ Youfilx/ViewController/DetailPageViewController.swift
@@ -55,6 +55,39 @@ final class DetailPageViewController: UIViewController {
         uiLabel.font = .systemFont(ofSize: 14, weight: .regular)
         return uiLabel
     }()
+    private lazy var videoMakerStackView = UIStackView().and {
+        $0.alignment = .center
+        $0.distribution = .fill
+        $0.axis = .horizontal
+        $0.spacing = 10
+        $0.backgroundColor = .black
+    }
+    private lazy var videoMakerImageView = UIImageView().and {
+        $0.layer.cornerRadius = 15
+    }
+    private lazy var videoMakerInformationStackView = UIStackView().and {
+        $0.distribution = .fillProportionally
+        $0.axis = .horizontal
+        $0.spacing = 10
+    }
+    private lazy var videoMakerNameLabel = UILabel().and {
+        $0.textColor = .white
+        $0.font = .systemFont(ofSize: 16, weight: .bold)
+    }
+    private lazy var videoMakerSubscriberCountLabel = UILabel().and {
+        $0.textColor = .white
+        $0.font = .systemFont(ofSize: 12, weight: .thin)
+    }
+    private lazy var videoLikeButton = ImageChangableWhenSelectedButton(
+        normalImage: .checkmark,
+        selectedImage: .add,
+        checked: { checked in
+        // 버튼 누르면 찜한 목록 추가 되도록
+    }).and {
+        $0.setTitle("1.6천", for: .normal)
+        $0.setTitleColor(UIColor.white, for: .normal)
+        $0.setTitleColor(UIColor.white.withAlphaComponent(0.5), for: .highlighted)
+    }
     
     private let videoId: String
     
@@ -75,8 +108,13 @@ extension DetailPageViewController {
 
         configure()
 
+        // 테스트 코드 추후에 삭제 요망
         videoTitleLabel.text = "타이틀"
         videoInformationLabel.text = "조회수 4.8만회 1년 전 #Lovely #Haruy"
+        
+        videoMakerImageView.image = .actions
+        videoMakerNameLabel.text = "Maker"
+        videoMakerSubscriberCountLabel.text = "3.2천"
         
     }
 }
@@ -88,7 +126,7 @@ extension DetailPageViewController {
         view.addSubview(scrollView)
         scrollView.addSubview(stackView)
 
-        [youtubeView, videoInformationStackView].forEach {
+        [youtubeView, videoInformationStackView, videoMakerStackView].forEach {
             stackView.addArrangedSubview($0)
         }
         [videoInformationLabel, videoMoreInformationLabel].forEach {
@@ -96,6 +134,12 @@ extension DetailPageViewController {
         }
         [videoTitleLabel, videoInformationsStackView].forEach {
             videoInformationStackView.addArrangedSubview($0)
+        }
+        [videoMakerNameLabel, videoMakerSubscriberCountLabel].forEach {
+            videoMakerInformationStackView.addArrangedSubview($0)
+        }
+        [videoMakerImageView, videoMakerInformationStackView, videoLikeButton].forEach {
+            videoMakerStackView.addArrangedSubview($0)
         }
         
         layout()
@@ -106,6 +150,9 @@ extension DetailPageViewController {
         scrollView.translatesAutoresizingMaskIntoConstraints = false
         stackView.translatesAutoresizingMaskIntoConstraints = false
         youtubeView.translatesAutoresizingMaskIntoConstraints = false
+        
+        videoMakerImageView.translatesAutoresizingMaskIntoConstraints = false
+        videoMakerNameLabel.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
@@ -119,7 +166,10 @@ extension DetailPageViewController {
             stackView.rightAnchor.constraint(equalTo: scrollView.rightAnchor),
             stackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
     
-            youtubeView.heightAnchor.constraint(equalToConstant: 300)
+            youtubeView.heightAnchor.constraint(equalToConstant: 300),
+            
+            videoMakerImageView.heightAnchor.constraint(equalToConstant: 30),
+            videoMakerImageView.widthAnchor.constraint(equalToConstant: 30)
         ])
     }
     
